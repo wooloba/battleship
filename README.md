de13d2560ee19150158c151bd591d6ca44b15eab
yaozhi <luyaozhiusing@gmail.com>
2020-03-01 23:36:11 -0700

    build

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Use `yarn start` to run the app in the development mode.
+Use `yarn start` or `node run.js` to run the app.
+
 Open http://localhost:3000 to view it in the browser.
---
+++ build/precache-manifest.c81bfd65b9a269b1388137bf1cfcd635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f62d018adb931c4cef86f503d4efb4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96ac165f1c92bb6c52d0",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "8fbdedbd03d8297c6d03",
+    "url": "/static/css/main.907070b4.chunk.css"
+  },
+  {
+    "revision": "96ac165f1c92bb6c52d0",
+    "url": "/static/js/2.b6d88dac.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/static/js/2.b6d88dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbdedbd03d8297c6d03",
+    "url": "/static/js/main.6d42c72b.chunk.js"
+  },
+  {
+    "revision": "e4e6b4ce2a73491882a3",
+    "url": "/static/js/runtime-main.369826bb.js"
+  }
+]);
